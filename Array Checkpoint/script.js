ae4ff30197778f8d196bd5ccca3bbd1542412ebc
Grant Morgan <MorganIII.Grant@gmail.com>
2020-03-30 11:09:56 -0400

    Array checkpoint coming up

---
+++ Array Checkpoint/script.js
@@ -0,0 +1,15 @@
+// .join() .splice() .pop() 
+
+var drinks =[];
+drinks[0] = 'pepsi';
+drinks[1] = 'coke';
+drinks[2] = 'other';
+
+var foods = ['apple' , 'sandwhich' , 'chips'];
+
+var meals = [drinks , foods ]
+
+
+
+
+
